78a77e9f732f61971cdff61ae8ee13905eb33029
andrewt-cville <andrewfthornton@gmail.com>
2024-07-16 21:32:28 -0400

    Add files via upload

---
+++ EVE_week1.R
@@ -0,0 +1,15 @@
+library(data.table)
+library(stargazer)
+library(ggplot2)
+# note: to read the date, you may need to set your working directory
+EVE_char <- fread('EVE_char.csv')
+
+# regressions
+m1 <- lm(LogonHours ~ CGender + CCareer + Uage_decile ,
+         data=EVE_char)
+m2 <- lm(LogonHours ~ CGender + CCareer + Uage_decile + CViolence + CCorp ,
+         data=EVE_char)
+# formatted output
+stargazer(m1,m2,single.row = FALSE,type="text",omit.stat = "f",ci=F)
+
+summary(m1)
